477c04be854d2da8319cb6b9a08569852c8a7241
Script-Java <atrindev@gmail.com>
2026-06-04 19:50:14 -0500

    fix: resolve syntax error in layout.jsx caused by escaped backticks in GTM script and className

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -151,19 +151,23 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {\`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5QSGHZL2');
-          \`}
-        </Script>
+        <Script 
+          id="google-tag-manager" 
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5QSGHZL2');
+            `
+          }}
+        />
         {/* End Google Tag Manager */}
       </head>
       <body
-        className={\`\${geistSans.variable} \${geistMono.variable} antialiased\`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {/* Google Tag Manager (noscript) */}
         <noscript>
